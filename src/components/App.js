9cba4f42272a41de23f22d3f3957ba37a595ff26
Tomer Harari <tharari21@gmail.com>
2022-07-23 08:19:28 -0400

    commit tree 858958bc1c6905c7ec13a05cf7b450fb3806240f

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+const App = () => {
+  const [dogImage, setDogImage] = useState("");
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message);
+      });
+  }, []);
+  //   if (!dogImage) return <p>Loading...</p>;
+
+  //   return <img src={dogImage} alt="A Random Dog" />;
+  return (
+    <div>
+      {dogImage ? <img src={dogImage} alt="A Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default App;
